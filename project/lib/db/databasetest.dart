562597af0363bf34150fddb95e375a8391a6b6b8
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2023-02-18 21:22:27 +0900

    オリジナルに戻す

--- project/lib/db/databasetest.dart
+++ project/lib/db/databasetest.dart
@@ -1,16 +1,15 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:project/db/database.dart';
 import 'package:logger/logger.dart';
 
 final logger = Logger();
 
-void main() => runApp(const DataBaseTestPage());
+void main() => runApp(const MyApp());
 
-class DataBaseTestPage extends StatelessWidget {
-  const DataBaseTestPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -66,11 +65,6 @@ class MyHomePage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.go('/'),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
